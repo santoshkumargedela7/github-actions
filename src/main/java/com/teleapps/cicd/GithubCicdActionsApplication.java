01ad18a68fad56309d275b1a20d85da7d92f3e7a
Siva Santhosh <sivasanthosh.g@teleapps.com>
2024-02-22 14:38:03 +0530

    second commit

--- src/main/java/com/teleapps/cicd/GithubCicdActionsApplication.java
+++ src/main/java/com/teleapps/cicd/GithubCicdActionsApplication.java
@@ -18,16 +18,4 @@ public class GithubCicdActionsApplication {
 		SpringApplication.run(GithubCicdActionsApplication.class, args);
 	}
 
-	
-	
-	
-	
-	
-//	echo "# github-actions" >> README.md
-//	git init
-//	git add README.md
-//	git commit -m "first commit"
-//	git branch -M main
-//	git remote add origin https://github.com/santoshkumargedela7/github-actions.git
-//	git push -u origin main
 }
